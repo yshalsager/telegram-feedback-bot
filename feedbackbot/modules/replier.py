b74bad99710c68805dfc6eaa3ac3f92c2182ff3b
yshalsager <ysh-alsager@hotmail.com>
2023-10-31 14:02:22 +0200

    chore(replier): use IS_TOPIC_MESSAGE filter
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- feedbackbot/modules/replier.py
+++ feedbackbot/modules/replier.py
@@ -8,15 +8,17 @@ from feedbackbot.db.curd import get_user_id_of_topic, increment_outgoing_stats
 async def reply_handler(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     assert update.effective_message is not None
     user_id = get_user_id_of_topic(update.effective_message.message_thread_id)
-    if user_id == -1:
-        return
     await update.effective_message.copy(user_id)
     increment_outgoing_stats()
 
 
+# TODO Message handler filters needs to be further tweaked to reply only on reply to a topic message
 application.add_handler(
     MessageHandler(
-        filters.REPLY & filters.Chat(chat_id=int(TELEGRAM_CHAT_ID)) & ~filters.COMMAND,
+        filters.IS_TOPIC_MESSAGE
+        & filters.REPLY
+        & filters.Chat(chat_id=int(TELEGRAM_CHAT_ID))
+        & ~filters.COMMAND,
         reply_handler,
     )
 )
