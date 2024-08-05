fc85a4caca663697e1cf4f9c4a4508378f94dba7
yshalsager <ysh-alsager@hotmail.com>
2024-08-05 18:19:15 +0300

    Add alembic migration
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- src/bot/db/scripts/migrate.py
+++ src/bot/db/scripts/migrate.py
@@ -1,17 +1,19 @@
 from alembic import command
 from alembic.config import Config
 
+from src import DATA_DIR, WORK_DIR
 from src.builder.db.crud import get_bots_ids
 
 
 def run_migrations(database_url: str) -> None:
-    alembic_cfg = Config('./feedbackbot/bot/db/alembic.ini')
+    alembic_cfg = Config(WORK_DIR / 'bot/db/alembic.ini')
     alembic_cfg.set_main_option('sqlalchemy.url', database_url)
+    # command.revision(alembic_cfg, 'update', autogenerate=True)
     command.upgrade(alembic_cfg, 'head')
 
 
 def main() -> None:
-    database_urls = [f'sqlite:///feedbackbot/data/{bot}.db' for bot in get_bots_ids()]
+    database_urls = [f'sqlite:///{DATA_DIR.absolute()}/{bot}.db' for bot in get_bots_ids()]
 
     for url in database_urls:
         run_migrations(url)
