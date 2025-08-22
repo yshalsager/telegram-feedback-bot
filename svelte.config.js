e15f15e9426b3aa10377f54683e437cdf241e467
yshalsager <ysh-alsager@hotmail.com>
2025-08-22 20:25:57 +0300

    chore: update SvelteKit configuration
    - Updated svelte.config.js to include output bundle strategy and added a TODO comment regarding the src directory.

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: 'index.html' // Enable SPA routing
-		}),
-		files: {
-			src: 'frontend'
-		},
-	}
-};
+    kit: {
+        adapter: adapter({
+            fallback: 'index.html' // Enable SPA routing
+        }),
+        files: {
+            src: 'frontend' // TODO: remove this after removing old src
+        },
+        output: {
+            bundleStrategy: 'single'
+        }
+    }
+}
 
-export default config;
+export default config
