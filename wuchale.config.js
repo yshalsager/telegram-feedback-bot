a75bdb1631b16f21cbda49cec5f1df8a0b4231c2
yshalsager <ysh-alsager@hotmail.com>
2025-09-27 23:38:31 +0300

    feat(i18n): add JavaScript adapter and loader for Wuchale

--- wuchale.config.js
+++ wuchale.config.js
@@ -1,6 +1,7 @@
 // @ts-check
 import {defineConfig} from 'wuchale'
 import {adapter as svelte} from '@wuchale/svelte'
+import {adapter as js} from 'wuchale/adapter-vanilla'
 
 export default defineConfig({
     sourceLocale: 'en',
@@ -10,6 +11,14 @@ export default defineConfig({
             files: ['./frontend/**/*.svelte', './frontend/**/*.svelte.{js,ts}'],
             catalog: './messages/{locale}/LC_MESSAGES/frontend',
             loaderPath: './frontend/lib/wuchale-loader.svelte.js'
+        }),
+        js: js({
+            files: [
+                // 'frontend/**/+{page,layout}.{js,ts}',
+                'frontend/**/lib/i18n.{js,ts}'
+            ],
+            catalog: './messages/{locale}/LC_MESSAGES/frontend',
+            loaderPath: './frontend/lib/wuchale-loader.js'
         })
     }
 })
