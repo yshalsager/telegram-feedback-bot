ddbbbfef6a25896026d254785046af24724691aa
yshalsager <ysh-alsager@hotmail.com>
2025-10-24 00:45:14 +0300

    chore: remove deprecated bot code

--- src/bot/db/__init__.py
+++
@@ -1,6 +0,0 @@
-from src.bot.db.models.chat import Chat
-from src.bot.db.models.mapping import Mapping
-from src.bot.db.models.stats import Stat
-from src.bot.db.models.topic import Topic
-
-__all__ = ['Chat', 'Mapping', 'Stat', 'Topic']
--- src/builder/db/__init__.py
+++
@@ -1,5 +0,0 @@
-from .models.bot import Bot
-from .models.user import User
-from .models.whitelist import Whitelist
-
-__all__ = ['Bot', 'User', 'Whitelist']
