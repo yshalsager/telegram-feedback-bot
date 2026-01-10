862836bb3d50531594ec1449b45f6195676bdd39
yshalsager <ysh-alsager@hotmail.com>
2026-01-10 19:47:02 +0200

    chore: add loader files to .gitignore

--- src/locales/js.loader.js
+++
@@ -1,8 +0,0 @@
-import {registerLoaders} from 'wuchale/load-utils'
-import {loadCatalog, loadIDs} from './.wuchale/js.proxy.js'
-
-const key = 'js'
-
-// two exports. can be used anywhere
-export const getRuntime = registerLoaders(key, loadCatalog, loadIDs)
-export const getRuntimeRx = getRuntime
